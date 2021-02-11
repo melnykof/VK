7d2bed841f2e06f8d74a882eb1ce7dadb1a78cdc
melnykof <melnykofart@gmail.com>
2021-02-11 20:35:00 +0200

    frist commit

---
+++ src/main/java/PostType.java
@@ -0,0 +1,65 @@
+public class PostType {
+    private int post;
+    private int copy;
+    private int reply;
+    private int postPone;
+    private int suggest;
+    private int likesInfo;
+    private int commentsInfo;
+
+    public int getLikesInfo() {
+        return likesInfo;
+    }
+
+    public void setLikesInfo(int likesInfo) {
+        this.likesInfo = likesInfo;
+    }
+
+    public int getCommentsInfo() {
+        return commentsInfo;
+    }
+
+    public void setCommentsInfo(int commentsInfo) {
+        this.commentsInfo = commentsInfo;
+    }
+
+    public int getPost() {
+        return post;
+    }
+
+    public void setPost(int post) {
+        this.post = post;
+    }
+
+    public int getCopy() {
+        return copy;
+    }
+
+    public void setCopy(int copy) {
+        this.copy = copy;
+    }
+
+    public int getReply() {
+        return reply;
+    }
+
+    public void setReply(int reply) {
+        this.reply = reply;
+    }
+
+    public int getPostPone() {
+        return postPone;
+    }
+
+    public void setPostPone(int postPone) {
+        this.postPone = postPone;
+    }
+
+    public int getSuggest() {
+        return suggest;
+    }
+
+    public void setSuggest(int suggest) {
+        this.suggest = suggest;
+    }
+}
